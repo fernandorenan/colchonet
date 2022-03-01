0107a36695f486382edb7bb5517a5442a89ef190
fernando.renan <fernando.renan@locaweb.com.br>
2022-03-01 15:17:08 -0300

    Início

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+    
+    def new
+        @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to @user,
+                        :notice => 'Cadastro criado com sucesso!'
+        else
+            render :new
+                   
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+        redirect_to @user, :notice => 'Cadastro atualizado com sucesso!'
+        else
+        render :edit
+        end
+    end
+
+
+
+    def user_params
+        params.require(:user).permit(:email, :full_name, :location, :password, :password_confirmation, :bio)
+      end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+
+      validates :email, :full_name, :location, presence:true
+      validates :bio, length:{minimum: 30 }, allow_blank: false
+      validates :email, format: {with:/\A[^@]+@([^@\.]+\.)+[^@\.]+\z/}
+      validates :email, uniqueness:true
+
+      has_secure_password
+
+end
