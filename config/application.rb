2a8337bc491f0e69b3041b1ed46f4caf801595c7
fernando.renan <fernando.renan@locaweb.com.br>
2022-03-03 20:02:23 -0300

    Adiciona tradução em Cadastro

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Colchonet
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-      config.time_zone = "Brasilia"
+      config.time_zone = 'Brasilia'
     # config.eager_load_paths << Rails.root.join("extras")
    
     # The default locale is :en and all translations from    
